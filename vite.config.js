47269cef99137f546f5d297d5e3236df69fd8009
IhorDobryanskiy <l48507100120@gmail.com>
2023-05-04 11:58:33 +0300

    try to fix deploy issue

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // main config object
+  root: 'src/', // change root path from 'project root' to 'project root/src'
   build: {
     // configure build options
+    outDir: '../dist', // change build folder to 'docs
     minify: 'esbuild' // minify build with 'esbuild
   },
   server: {
@@ -13,5 +15,6 @@ export default defineConfig({
     open: true, // open on start
     port: 8383 // port
   },
-  plugins: [react()] // vite plugins
+  plugins: [react()], // vite plugins
+  base: '/final_project/'
 });

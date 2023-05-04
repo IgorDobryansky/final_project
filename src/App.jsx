cbf40c36691c3c763e74e758e48ca1293e3358cc
buzarig <lircusgaming@gmail.com>
2023-05-04 21:55:45 +0300

    add lol func

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import './assets/styles/style.scss';
+import React from "react";
+import "./assets/styles/style.scss";
+
+function Lol() {
+  console.log("kek");
+}
 
 function App() {
-  return <div>Базовая сборка проекта (:</div>;
+  return <div>Базовая сборка проекта </div>;
 }
 
 export default App;

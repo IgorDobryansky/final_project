c54968f8814eca5e323f0a6ed3cfd72f04aa41d9
IhorDobryanskiy <l48507100120@gmail.com>
2023-05-04 11:52:42 +0300

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // main config object
-  root: 'src', // change root path from 'project root' to 'project root/src'
   build: {
     // configure build options
-    outDir: '../dist', // change build folder to 'docs
     minify: 'esbuild' // minify build with 'esbuild
   },
   server: {

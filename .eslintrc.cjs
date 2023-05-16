bd3db49382e02d6c6ff5fcf16568a9bec9b1f7da
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-05-16 21:58:14 +0300

    add prettier & husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['airbnb', 'airbnb/hooks'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  extends: ["airbnb", "airbnb/hooks", "prettier"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
   plugins: [],
   rules: {
-    'arrow-body-style': ['error', 'as-needed'],
-    'linebreak-style': ['error', 'unix'],
-    'comma-dangle': ['error', 'never'],
-    'operator-linebreak': ['error', 'after'],
-    'react/self-closing-comp': [
-      'error',
+    "arrow-body-style": ["error", "as-needed"],
+    "linebreak-style": ["error", "unix"],
+    "comma-dangle": ["error", "never"],
+    "operator-linebreak": ["error", "after"],
+    "react/self-closing-comp": [
+      "error",
       {
         component: true,
         html: true
--- src/services/UserService.js
+++
@@ -1,7 +0,0 @@
-import $api from '../http';
-
-export default class UserService {
-  static fetchUsers() {
-    return $api.get('/users');
-  }
-}

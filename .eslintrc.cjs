5b4fd5e08d56dab31bd7d91e375ec57f7268e3e3
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-05-16 22:11:40 +0300

    another code-style fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  extends: ["prettier", "airbnb", "airbnb/hooks"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
   plugins: [],
   rules: {
-    'arrow-body-style': ['error', 'as-needed'],
-    'linebreak-style': ['error', 'unix'],
-    'comma-dangle': ['error', 'never'],
-    'operator-linebreak': ['error', 'after'],
-    'react/self-closing-comp': [
-      'error',
+    quotes: ["error", "double"],
+    "arrow-body-style": ["error", "as-needed"],
+    "linebreak-style": ["error", "unix"],
+    "comma-dangle": ["error", "never"],
+    "operator-linebreak": ["error", "after"],
+    "react/self-closing-comp": [
+      "error",
       {
         component: true,
-        html: true,
-      },
-    ],
-  },
+        html: true
+      }
+    ]
+  }
 };

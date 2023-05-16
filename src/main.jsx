5b38d8b2d9d0a42b4015fa5bae5e8680689fcd7b
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-05-16 22:06:44 +0300

    fix prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import store from './redux/store';
-import App from './App';
+import store from "./redux/store";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Provider store={store}>
       <App />

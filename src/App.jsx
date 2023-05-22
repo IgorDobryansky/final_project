05e749b56a1c77ccba6d924fe8bac268f59f9af7
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-05-22 22:09:20 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,6 @@
 import React from "react";
-// import axios from "axios";
-
-// export const API_URL = "https://final-project-fe-10.onrender.com/api";
-
-// const api = axios.create({
-//   mode: "no-cors",
-//   baseURL: API_URL
-// });
 
 function App() {
-  // useEffect(() => {
-  // const newCustomer = {
-  //   firstName: "Customer",
-  //   lastName: "Newone",
-  //   login: "Customer",
-  //   email: "customer@gmail.com",
-  //   password: "1111111",
-  //   telephone: "+380630000000",
-  //   gender: "male",
-  //   avatarUrl: "img/customers/023648.png",
-  //   isAdmin: true
-  // };
-  // const login = {
-  //   loginOrEmail: "l48507100120@gmail.com",
-  //   password: "1111111"
-  // };
-  // const newProduct = {
-  //   name: "new product for testing purposes",
-  //   currentPrice: 199.99,
-  //   previousPrice: 250,
-  //   categories: "men",
-  //   imageUrls: [
-  //     "img/products/men/001.png",
-  //     "img/products/men/002.png",
-  //     "img/products/men/003.png",
-  //     "img/products/men/004.png"
-  //   ],
-  //   quantity: 100,
-  //   color: "red",
-  //   productUrl: "/men",
-  //   brand: "braaaand",
-  //   myCustomParam: "some string or json for custom param"
-  // };
-  // api.post("/customers/login", login).then((res) => {
-  //   localStorage.setItem("token", res.data.token);
-  //   console.log(res);
-  // });
-  // }, []);
   return <div>123</div>;
 }
 

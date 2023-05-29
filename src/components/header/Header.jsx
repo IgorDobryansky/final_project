02312ef286fcffa855eeccd1390137231d644dd8
buzarig <lircusgaming@gmail.com>
2023-05-29 19:29:04 +0300

    styling header & added images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import "./header.scss";
+import mainLogo from "./images/main-logo.png";
+import heartLogo from "./images/heart-logo.png";
+import cabinetLogo from "./images/cabinet-logo.png";
+import carttLogo from "./images/shopping-cart-logo.png";
 
 function Header() {
   return (
     <div className="header">
-      <Link className="logo" to="/final_project">
-        Logo
-      </Link>
-      <div className="nav">
-        <ul>
-          <li>
-            <Link to="/final_project">Home</Link>
-          </li>
-          <li>
-            <Link to="/final_project/catalog">Каталог</Link>
-          </li>
-          <li>
-            <Link to="/final_project/delivery">Доставка</Link>
-          </li>
-          <li>
-            <Link to="/final_project/aboutUs">Про нас</Link>
-          </li>
-          <li>
-            <Link to="/final_project/contacts">Контакти</Link>
-          </li>
-        </ul>
+      <div className="header-wrapper">
+        <Link className="logo" to="/final_project">
+          <img className="logo-element" src={mainLogo} alt="" />
+        </Link>
+        <div className="nav">
+          <ul className="nav-list">
+            <li>
+              <Link to="/final_project">Home</Link>
+            </li>
+            <li>
+              <Link to="/final_project/catalog">Каталог</Link>
+            </li>
+            <li>
+              <Link to="/final_project/delivery">Доставка</Link>
+            </li>
+            <li>
+              <Link to="/final_project/aboutUs">Про нас</Link>
+            </li>
+            <li>
+              <Link to="/final_project/contacts">Контакти</Link>
+            </li>
+          </ul>
+
+          <ul className="nav-other">
+            <li>
+              <Link to="/final_project">
+                <img src={heartLogo} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/final_project">
+                <img src={cabinetLogo} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/final_project">
+                <div>
+                  <span>num</span>
+                  <img src={carttLogo} alt="" />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );

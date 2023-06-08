6e976ca41157d30bedb202f80b6761242763f49f
buzarig <lircusgaming@gmail.com>
2023-06-08 16:32:51 +0300

    completed work on the header and footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 import "../../styles/_footer.scss";
 
 import mainLogo from "../../assets/images/footer/main-logo.png";
 
+import twitterLogo from "../../assets/images/footer/twitter-logo.png";
+import facebookLogo from "../../assets/images/footer/facebook-logo.png";
+import instagramLogo from "../../assets/images/footer/instagram-logo.png";
+
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="footer">
       <div className="footer-wrapper">
@@ -14,24 +23,63 @@ function Footer() {
           <p>© 2021 “Copper Pro” Всі права захищені</p>
           <Link to="/final_project/privacy">Політика конфіденційності</Link>
         </div>
-        <div className="nav">
-          <ul>
-            <li>
-              <Link to="/final_project">Home</Link>
-            </li>
-            <li>
-              <Link to="/final_project/catalog">Каталог</Link>
-            </li>
-            <li>
-              <Link to="/final_project/delivery">Доставка</Link>
-            </li>
-            <li>
-              <Link to="/final_project/aboutUs">Про нас</Link>
-            </li>
-            <li>
-              <Link to="/final_project/contacts">Контакти</Link>
-            </li>
-          </ul>
+        <div className="navbar">
+          <div className="nav">
+            <h1 className="nav__title">Навігація</h1>
+            <ul>
+              <li>
+                <Link to="/final_project/catalog">Каталог</Link>
+              </li>
+              <li>
+                <Link to="/final_project/news">Новини</Link>
+              </li>
+              <li>
+                <Link to="/final_project/delivery">Доставка</Link>
+              </li>
+              <li>
+                <Link to="/final_project/aboutUs">Про нас</Link>
+              </li>
+              <li>
+                <Link to="/final_project/contacts">Контакти</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="contacts">
+            <h1 className="contacts__title">Контакти</h1>
+            <ul>
+              <li>Бажана 8-Б, Київ, 02132 Україна</li>
+              <li>+38 (096) 990 67 56</li>
+              <li>svoyachki@gmail.com</li>
+              <li>
+                <a href="https://www.twitter.com/">
+                  <img
+                    className="contacts-list__item-img"
+                    src={twitterLogo}
+                    alt=""
+                  />
+                </a>
+                <a href="https://www.facebook.com/">
+                  <img
+                    className="contacts-list__item-img facebook-img"
+                    src={facebookLogo}
+                    alt=""
+                  />
+                </a>
+                <a href="https://www.instagram.com/">
+                  <img
+                    className="contacts-list__item-img"
+                    src={instagramLogo}
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="ontop-wrapper">
+            <Button onClick={scrollToTop} variant="contained" color="primary">
+              ВВЕРХ
+            </Button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import "../../styles/_header.scss";
@@ -6,9 +6,32 @@ import "../../styles/_header.scss";
 import mainLogo from "../../assets/images/header/main-logo.png";
 import heartLogo from "../../assets/images/header/heart-logo.png";
 import cabinetLogo from "../../assets/images/header/cabinet-logo.png";
-import carttLogo from "../../assets/images/header/shopping-cart-logo.png";
+import cartLogo from "../../assets/images/header/shopping-cart-logo.png";
+
+import menuBtn from "../../assets/images/header/header-menu-btn.png";
 
 function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div className="header">
       <div className="header-wrapper">
@@ -18,13 +41,10 @@ function Header() {
         <div className="nav">
           <ul className="nav-list">
             <li>
-              <Link to="/final_project">Home</Link>
-            </li>
-            <li>
               <Link to="/final_project/catalog">Каталог</Link>
             </li>
             <li>
-              <Link to="/final_project/delivery">оставка</Link>
+              <Link to="/final_project/delivery">Доставка</Link>
             </li>
             <li>
               <Link to="/final_project/aboutUs">Про нас</Link>
@@ -33,28 +53,58 @@ function Header() {
               <Link to="/final_project/contacts">Контакти</Link>
             </li>
           </ul>
-
-          <ul className="nav-other">
+        </div>
+        <div className="pages">
+          <ul className="pages-list">
             <li>
-              <Link to="/final_project">
+              <Link to="/final_project/favorite">
                 <img src={heartLogo} alt="" />
               </Link>
             </li>
             <li>
-              <Link to="/final_project">
+              <Link to="/final_project/cabinet">
                 <img src={cabinetLogo} alt="" />
               </Link>
             </li>
             <li>
-              <Link to="/final_project">
+              <Link to="/final_project/cart">
                 <div>
                   <span>num</span>
-                  <img src={carttLogo} alt="" />
+                  <img src={cartLogo} alt="" />
                 </div>
               </Link>
             </li>
           </ul>
         </div>
+        <div className={`menu ${isMenuOpen ? "open" : ""}`} ref={menuRef}>
+          <button onClick={toggleMenu} type="button">
+            <img src={menuBtn} alt="" />
+          </button>
+          {isMenuOpen && (
+            <ul className="menu-list">
+              <li>
+                <Link to="/final_project/catalog">Каталог</Link>
+              </li>
+              <li>
+                <Link to="/final_project/delivery">Доставка</Link>
+              </li>
+              <li>
+                <Link to="/final_project/aboutUs">Про нас</Link>
+              </li>
+              <li>
+                <Link to="/final_project/contacts">Контакти</Link>
+              </li>
+              <li>
+                <Link to="/final_project/cart">Корзина</Link>
+              </li>
+              <p>
+                « Ми вчимо насолоджуватися кожною хвилиною »
+                <span>Степан Бандера</span>
+              </p>
+            </ul>
+          )}
+        </div>
+        {isMenuOpen && <div className="overlay" />}
       </div>
     </div>
   );

43fd97e276684c959ffefe3ebd199de8f554392f
buzarig <lircusgaming@gmail.com>
2023-06-12 21:32:46 +0300

    completed header and footer(repeat)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,27 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 
+import "../styles/_home.scss";
+
 function Home() {
   return (
     <div className="container">
       <Header />
-      <main>
+      <main className="main">
         <Outlet />
+        <div className="about-us">
+          <div className="about-us-wrapper">
+            <h1>Чому обирають нас</h1>
+            <div className="about-us__container">
+              <div className="about-us__container-item">a</div>
+              <div className="about-us__container-item">s</div>
+              <div className="about-us__container-item">e</div>
+              <div className="about-us__container-item">r</div>
+              <div className="about-us__container-item">t</div>
+              <div className="about-us__container-item">y</div>
+            </div>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>

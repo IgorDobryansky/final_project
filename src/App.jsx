3390430a6c2e9511985b3fe81f2dcebdb8b4bbcf
Maxim Sidletskiy <sidmax2004@gmail.com>
2023-06-22 19:57:49 +0200

    add product

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { Route, Routes, Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-
 import AboutUs from "./pages/AboutUs";
 import Catalog from "./pages/Catalog";
-import Product from "./components/product/Product";
 import Home from "./pages/Home";
 import Delivery from "./pages/Delivery";
 import Contacts from "./pages/Contacts";
 import Order from "./pages/Order";
 import Basket from "./pages/Basket";
-import Thanks from "./pages/Thanks";
 import RegisterForm from "./pages/Register";
 import LoginForm from "./pages/Login";
 

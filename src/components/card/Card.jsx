98b7ad3e4d353c97f38a629d57fde00e5ffa44a7
Mykola Batrak <kolyaun@live.com>
2023-06-22 23:36:06 +0300

    fixed style for card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,12 +8,12 @@ function Card({ product }) {
       <img className="card__image" src={product.img} alt="Product" />
       <div className="card__description">
         <h1>{product.name}</h1>
-        <div className="price">
+        <div className="card__price">
           {product.priceOld && (
-            <h2 className="price__old">{product.priceOld} грн</h2>
+            <h2 className="card__price__old">{product.priceOld} грн</h2>
           )}
           <p
-            className={product.priceOld ? "price__now promotion" : "price__now"}
+            className={product.priceOld ? "card__price__now promotion" : "card__price__now"}
           >
             {product.priceNow} грн
           </p>

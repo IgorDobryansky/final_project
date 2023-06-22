3b68642c13d05ce6bdbcd4e357eb83ae5dde2fc9
Mykola Batrak <kolyaun@live.com>
2023-06-22 23:23:23 +0300

    added select component

---
+++ src/components/select/Select.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./_select.scss";
+
+function Select() {
+  return (
+    <div>
+      <label className="select__label" for="cars">Сортування:</label>
+      <select name="cars" id="cars">
+        <option value="null">Інше</option>
+        <option value="minPrice">Від дешевих</option>
+        <option value="maxPrice">Від дорогих</option>
+      </select>
+    </div>
+  );
+}
+
+export default Select;

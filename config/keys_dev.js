efd326e43aae629b9682ef9bea8f3791af2dec02
buzarig <lircusgaming@gmail.com>
2023-07-07 00:30:50 +0300

    added backend

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://kolya:7W1xKuGtgvd7qOBq@clustershop.cdtfk2a.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
+};

88fa7e2e54271be6c33e22a4fd68bf7a2531f494
yanabilous <yanabilous05@gmail.com>
2023-08-17 17:57:16 +0300

    added for Product btn handleAddToCart to basket

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
 import { merchandiseReducer as merchandise } from "./reducers/merchandise";
 import basketReducer from "./basket/reducer";
-import catalogReducer from "./catalog/reducer";
+// import catalogReducer from "./catalog/reducer";
 
 const rootReducer = combineReducers({
   merchandise,
-  basket: basketReducer,
-  catalog: catalogReducer
+  basket: basketReducer
+  // catalog: catalogReducer
 });
 
 export default rootReducer;
